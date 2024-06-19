3c22b7600c7c53e3ded7d64b0360f3d18f35cb28
kathroji-sai-yashwanth <saiyashwanth836@gmail.com>
2024-06-19 22:34:31 +0530

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,49 @@
+let i
+function buttonsnine(){
+  let texting=document.getElementById('texting')
+  texting.value+='9';
+}
+function buttonseight(){
+  let texting=document.getElementById('texting')
+  texting.value +='8';
+}
+function buttonsseven(){
+  let texting=document.getElementById('texting')
+  texting.value +='7';
+}
+function buttonsadd(){
+  let texting=document.getElementById('texting')
+  texting.value+='+';
+}
+function buttonssix(){
+  let texting=document.getElementById('texting')
+  texting.value+=6;
+}
+function buttonsfive(){
+  let texting=document.getElementById('texting')
+  texting.value+=5;
+}
+function buttonsfour(){
+  let texting=document.getElementById('texting')
+  texting.value+=4;
+}
+function buttonssub(){
+  let texting=document.getElementById('texting')
+  texting.value+='-';
+}
+function buttonsthree(){
+  let texting=document.getElementById('texting')
+  texting.value+=3;
+}
+function buttonstwo(){
+  let texting=document.getElementById('texting')
+  texting.value+=2;
+}
+function buttonsone(){
+  let texting=document.getElementById('texting')
+  texting.value+=1;
+}
+function buttonsmul(){
+  let texting=document.getElementById('texting')
+  texting.value+='*';
+}
